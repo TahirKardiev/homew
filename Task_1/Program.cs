da7558457992f734e58668d042ea861aa723cf92
Andrei <mr.fesenko@inbox.ru>
2022-08-08 15:57:39 +0300

    add task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿{
+double a, b; string clir;
+{
+Console.Write("Введите число A : "); clir = Console.ReadLine();
+a = double.Parse(clir);
+Console.Write("Введите число B : "); clir = Console.ReadLine();
+b = double.Parse(clir);
+if (a > b)
+{  
+Console.WriteLine($"{a}");
+}
+else if (a < b)
+{ 
+Console.WriteLine($"{b}");
+}
+else
+{ 
+Console.WriteLine("the numbers are equal");
+}
+Console.Write("Нажмите любую клавишу ..."); Console.ReadKey(true);
+}
+}
