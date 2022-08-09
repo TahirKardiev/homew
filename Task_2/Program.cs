faa9c5d61f3e4b2bccf9f40b522e161ada16d9e9
Andrei <mr.fesenko@inbox.ru>
2022-08-09 17:29:35 +0300

    add editions to task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿{
+double a, b, c; string clir; double max; 
+{
+Console.Write("Введите число A : "); clir = Console.ReadLine();
+a = double.Parse(clir);
+Console.Write("Введите число B : "); clir = Console.ReadLine();
+b = double.Parse(clir);
+Console.Write("Введите число C : "); clir = Console.ReadLine();
+c = double.Parse(clir);
+if (a >= b)
+ {
+    if (c >= a)
+    {
+       max = c;
+    }
+    else
+    {
+       max = a;
+    }
+ }
+ else
+ {
+    if (c >= b)
+    {
+       max = c;
+    }
+    else
+    {
+       max = b;
+    }
+ }
+Console.WriteLine($" {max} "); 
+}
+Console.Write("Нажмите любую клавишу ..."); Console.ReadKey(true);
+}
