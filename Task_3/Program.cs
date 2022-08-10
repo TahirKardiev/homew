24d62e7e34e30122e46001c75bfc79cf0a1b1f49
Andrei <mr.fesenko@inbox.ru>
2022-08-10 11:33:45 +0300

    add Task3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿string clir; double a;
+Console.WriteLine ("Enter a number "); clir = Console.ReadLine();
+a = double.Parse(clir);
+if (a % 2 == 1)
+{
+    Console.WriteLine ("odd");
+}
+else
+{
+    Console.WriteLine ("even");
+}
