5b43f363f6efde082e6d8786f80bafc5555a5af9
Andrei <mr.fesenko@inbox.ru>
2022-08-10 14:33:43 +0300

    add Task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿double a, i = 1; string clir;
+Console.WriteLine ("Enter a number "); clir = Console.ReadLine ();
+a = double.Parse (clir);
+while (i <= a)
+ { 
+   if(i % 2 == 0) 
+   {
+        Console.WriteLine(i);
+   }
+    i = i + 1;
+ }
